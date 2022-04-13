815438c245bcd3ba5ebd6fb7b6e760ef9fee99a8
Jacob Quant <jacobq@gmail.com>
2022-04-13 18:12:09 -0500

    WIP: try wrapping require; no dice

--- tests/vitest-setup.js
+++ tests/vitest-setup.js
@@ -21,4 +21,31 @@ writeFileSync(setupScript, [
   //readFileSync(`${distPath}/assets/test-support.js`, 'utf-8'),
 ].join('\r\n')); // sadly this obviously breaks source mapping...
 
+// Taken from https://github.com/adopted-ember-addons/ember-electron/blob/main/vendor/wrap-require.js
+((win) => {
+  const { requireNode, require: requireAMD } = win;
+
+  // Redefine a global `require` function that can satisfy both Node and AMD module systems.
+  if (requireAMD) {
+    win.require = (...args) => {
+      try {
+        return requireAMD(...args);
+      } catch(error) {
+        if (error.toString().includes('Error: Could not find module')) {
+          return requireNode(...args);
+        }
+
+        console.error(error); // eslint-disable-line no-console
+      }
+    };
+
+    // Both Ember's and Node's require() have some properties (e.g. Ember's has
+    // require.has() and Node's has require.resolve()). Fortunately there are no
+    // naming conflicts, so we can expose them all.
+    Object.assign(win.require, requireNode, requireAMD);
+  } else {
+    win.require = requireNode;
+  }
+})(globalThis.window);
+
 require(setupScript); // Error: Could not find module `@ember/application` imported from `dummy/app`
